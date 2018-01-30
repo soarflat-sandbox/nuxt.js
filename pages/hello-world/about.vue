e67880cdbeee8926d40471275364b346f9ee6c54
soarflat <soarflat777@gmail.com>
2018-01-30 21:19:27 +0900

    feat: Hello Worldのデモを追加

---
+++ pages/hello-world/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Hi from {{ name }}</p>
+    <nuxt-link to="/hello-world/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    // asyncDataはページコンポーネントがロードされる前に毎回呼び出されるメソッド
+    // サーバーサイドレンダリングや、ユーザーがページ遷移する前にも呼び出される
+    // このメソッドは第一引数としてcontext（https://ja.nuxtjs.org/api/context）を受取り
+    // contextを利用し、データを取得してコンポーネントのデータを返す
+    asyncData() {
+      return {
+        name: process.static ? 'static' : (process.server ? 'server' : 'client')
+      }
+    },
+    head: {
+      title: 'About page'
+    }
+  }
+</script>
---
+++ pages/hello-world/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <nuxt-link to="/hello-world/about">About page</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Home page'
+    }
+  }
+</script>
